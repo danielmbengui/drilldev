aef9dc9848f0fab4e77b627676a8449649ca77e1
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-02-26 01:55:42 +0100

    create virtual .json on webp file to push it to github

--- pages/api/pictures.js
+++ pages/api/pictures.js
@@ -4,11 +4,11 @@ import initMiddleware from '@/lib/init-middleware';
 import { cwd } from 'process';
 import path from 'path';
 import { getFirstLetterUpperCase, getOnePictureFromList, getRelativePath, getListPictures } from './constants';
-import { DIR_MIDJOURNEY_DATAS, DIR_WEBP, GALLERY_MAX_PICTURES_PER_PAGE, METHOD_GET, METHOD_POST, QUERY_ACTION_GET_LIST_PICTURES, QUERY_PAGE, QUERY_PER_PAGE, QUERY_SEARCH, } from '@/constants';
+import { DIR_MIDJOURNEY_DRAFTS, DIR_MIDJOURNEY_DATAS, DIR_WEBP, GALLERY_MAX_PICTURES_PER_PAGE, METHOD_GET, METHOD_POST, QUERY_ACTION_GET_LIST_PICTURES, QUERY_PAGE, QUERY_PER_PAGE, QUERY_SEARCH, } from '@/constants';
 import getConfig from 'next/config';
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
-const DIR_MIDJOURNEY_DRAFTS = `${path.join(publicRuntimeConfig.staticFolder, 'images', 'midjourney', 'drafts')}`;
+//const DIR_MIDJOURNEY_DRAFTS = path.join('ok', 'images');
 const PATH_PICTURES = `${process.cwd()}/public/datas/images/`;
 const sharp = require('sharp');
 
