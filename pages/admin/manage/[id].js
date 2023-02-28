39f09fc07b55bb74a20700958c47d7fcae9e58ab
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-02-28 04:28:16 +0100

    add page for management

--- pages/admin/manage/[id].js
+++ pages/admin/manage/[id].js
@@ -1,45 +1,26 @@
 import Head from 'next/head'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { NAMESPACE_LANGAGE_COMMON, TAB_LANGAGES, TAB_NAMEPACES } from '@/constants';
+import { TAB_LANGAGES, TAB_NAMEPACES } from '@/constants';
 import { useTranslation } from 'next-i18next';
-import ContainerPageComponent from '@/components/Containers/ContainerPageComponent';
 import { Text, useSSR, useTheme } from '@nextui-org/react';
-import ManageComponent from '@/components/All/ManageComponent';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function ManagePage(props) {
   const {isDark} = useTheme();
-    const {lang, setLang, isMobile, isTablet, isLaptop} = props;
   const {t} = useTranslation(TAB_NAMEPACES);
 
   const router = useRouter();
   const { id } = router.query
-  const [content, setContent] = useState(<></>);
 
   useEffect(() => {
     console.log("IIIIIID", id)
   })
   return (
-    <ContainerPageComponent
-    isMobile={isMobile} isTablet={isTablet} isLaptop={isLaptop}
-        title={
-          <Text h1 size={45} b css={{
-            textGradient: `45deg, $${isDark ? 'white' : 'black'} -20%, $orange600 100%, $orange600 100%`,
-          }}>
-    {`En parcourant la galerie, vous pourrez découvrir les différentes créations réalisées par nos soins`}
-        </Text>}
-    lang={lang} setLang={setLang}
-    >
-      <Head>
-        <title>{`${t('menuGallery', {ns:NAMESPACE_LANGAGE_COMMON})}`}</title>
-        <meta name="description" content="Bienvenue sur notre site consacré à la démonstration d'illustrations générées par intelligence artificielle. Nous vous montrons les dernières tendances et techniques de génération d'images à l'aide de l'IA. Vous découvrirez les meilleures plateformes et outils pour créer vos propres illustrations de qualité, ainsi que les mots clés à utiliser pour optimiser les résultats. Suivez notre guide étape par étape pour créer vos propres illustrations surprenantes avec l'IA. Rejoignez notre communauté pour partager vos créations et découvrir celles des autres utilisateurs." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      {content}
-<ManageComponent lang={lang} isMobile={isMobile} />
-    </ContainerPageComponent>
+    <>
+    {`_IIIIIIID: ${id}`}
+    </>
   )
 }
 
